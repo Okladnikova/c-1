c901cdb6c1ce4f09e96fee70633295a645367a02
Irina <okladnikova.iv@yandex.ru>
2022-05-18 01:14:45 +0800

    задача4

---
+++ задача4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать четные числа от 1 до N
+Console.WriteLine("Ввведите число верхней границы отрезка: N");
+int N = Convert.ToInt32(Console.ReadLine());
+int i=1;
+while (i<N)
+{
+ if (i%2==0)
+{
+        Console.WriteLine(i);
+}
+i++;
+}
